9876a508fe2b4db44c5446a3446eeefd086e432e
MHPranajaya <mhpranajaya1@Muhammads-MacBook-Pro.local>
2018-12-30 19:16:00 +0700

    bound service

---
+++ app/src/main/java/id/mhpranajaya/serviceapp/BoundService.java
@@ -0,0 +1,70 @@
+package id.mhpranajaya.serviceapp;
+
+import android.app.Service;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.util.Log;
+
+public class BoundService extends Service {
+
+    final String TAG = BoundService.class.getSimpleName();
+
+    MyBinder mBinder = new MyBinder();
+    final long startTime = System.currentTimeMillis();
+
+    class MyBinder extends Binder {
+        BoundService getService(){
+            return BoundService.this;
+        }
+    }
+
+    CountDownTimer mTImer = new CountDownTimer(100000, 1000) {
+        @Override
+        public void onTick(long l) {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            Log.d(TAG, "onTick: "+elapsedTime);
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
+    };
+
+    public BoundService() {
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy:");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind: ");
+        mTImer.start();
+        return mBinder;
+    }
+
+    public boolean onUnbind(Intent intent){
+        Log.d(TAG, "onUnBind: ");
+        mTImer.cancel();
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent){
+        super.onRebind(intent);
+        Log.d(TAG, "onRebind: ");
+    }
+}
--- app/src/main/java/id/mhpranajaya/serviceapp/MainActivity.java
+++ app/src/main/java/id/mhpranajaya/serviceapp/MainActivity.java
@@ -1,6 +1,9 @@
 package id.mhpranajaya.serviceapp;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +15,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnStartIntentService;
     Button btnStartBoundService;
     Button btnStopBoundService;
+    boolean mServiceBound =false;
+    BoundService mBoundService;
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +46,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnStartBoundService.setOnClickListener(this);
         btnStopBoundService = findViewById(R.id.btn_stop_bound_service);
         btnStopBoundService.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if (mServiceBound){
+            unbindService(mServiceConnection);
+        }
     }
 
     @Override
@@ -44,9 +72,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btn_start_bound_service:
+                Intent mBoundServiceIntent = new Intent(MainActivity.this, BoundService.class);
+                bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
                 break;
 
             case R.id.btn_stop_bound_service:
+                unbindService(mServiceConnection);
                 break;
         }
 
